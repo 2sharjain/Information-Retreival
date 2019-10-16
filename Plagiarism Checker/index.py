c795e2c39a55a03098bb199d37580d1c509176a5
Naman Arora <xavier.arora@gmail.com>
2019-10-17 00:42:58 +0530

    add log messages

--- Plagiarism Checker/index.py
+++ Plagiarism Checker/index.py
@@ -18,13 +18,17 @@ class Index:
             self.load_minhash()
 
     def _build_lsh(self, minhashes):
+        print("Building LSH")
         self.lsh = LSH(minhashes)
         with open(self.LSH_FILE, 'wb') as f:
             pickle.dump(self.lsh.hash_tables, f)
 
+        print("Built LSH Successfully")
+
     def build_index(self):
         self.minhashes = {}
 
+        print("Building Minhashes")
         for file_name in os.listdir(self.CORPUS_DIR):
             file_name = os.path.join(self.CORPUS_DIR, file_name)
 
@@ -36,6 +40,7 @@ class Index:
         with open(self.MINHASH_FILE, 'wb') as f:
             pickle.dump(self.minhashes, f)
 
+        print("Successfully build minhashes")
         self._build_lsh(self.minhashes)
 
     def load_minhash(self):
