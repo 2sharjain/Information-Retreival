6ee7080f6d7adc5e99b1be88b33d3e098f0e7091
Naman Arora <xavier.arora@gmail.com>
2019-10-20 21:04:33 +0530

    add querying using TF IDF

--- Plagiarism Checker/main.py
+++ Plagiarism Checker/main.py
@@ -2,7 +2,7 @@ import json
 import os
 import argparse
 
-from preprocess import vectorize_file
+from preprocess import vectorize, process_doc
 from minhash import Minhash
 from index import Index
 from lsh import LSH
@@ -10,21 +10,36 @@ from lsh import LSH
 
 class Handler:
     def __init__(self):
-        self.config = {
-            "LSH_FILE": "lsh.pkl",
-            "MINHASH_FILE": "min.pkl",
-            "CORPUS_DIR": "./corpus"
-        }
-
+        self.config = self.get_config()
         self.index = Index(self.config)
-        self.lsh = self.index.lsh
-        self.minhashes = self.index.minhashes
 
     def set_config(self, key, value):
         self.config[key] = value
 
-    def query(self, file_name):
-        vector = vectorize_file(file_name)
+    def query(self, file_name, use_idf=False):
+        with open(file_name, 'r') as f:
+            text = f.read()
+            return self.query_using_idf(text) if use_idf else self.query_using_lsh(text)
+
+    def query_using_idf(self, text):
+        self.idf = self.index.idf
+        vector = self.idf.to_vector(process_doc(text))
+        docs = list(self.idf.vectors.keys())
+        vectors = self.idf.vectors
+
+        docs.sort(key=lambda x: self.idf.cosine(
+            vector, vectors[x]), reverse=True)
+
+        res = docs[:10]
+        res = [(doc, self.idf.cosine(vector, vectors[doc])) for doc in res]
+
+        return res
+
+    def query_using_lsh(self, text):
+        self.lsh = self.index.lsh
+        self.minhashes = self.index.minhashes
+
+        vector = vectorize(text)
         minhash = Minhash(vector)
         similarItems = self.lsh.get_similar(minhash)
 
@@ -37,6 +52,10 @@ class Handler:
 
         return [(doc, minhash.compare(self.minhashes[doc]) * 100) for doc in res]
 
+    def get_config(self):
+        with open('config.json', 'r') as f:
+            return json.load(f)
+
 
 if __name__ == '__main__':
     handler = Handler()
@@ -46,13 +65,15 @@ if __name__ == '__main__':
         "--file_name", help="Relative path to the file which you want to check", type=str)
     parser.add_argument(
         '--build', help='Build the LSH and minhash from scratch', action="store_true")
+    parser.add_argument(
+        '--use_idf', help='use TF-IDF to get similar', action='store_true')
 
     args = parser.parse_args()
 
     if args.build:
         handler.index.build_index()
     elif args.file_name:
-        results = handler.query(args.file_name)
+        results = handler.query(args.file_name, args.use_idf)
 
         for item in results:
             print("{} {}".format(*item))
